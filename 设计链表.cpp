0f5e52755e1e673e944d73dbdda9c37f46de6598
lose_sea <2031938936@qq.com>
2025-12-11 13:07:48 +0800

    Update 设计链表.cpp

--- 设计链表.cpp
+++ 设计链表.cpp
@@ -7,10 +7,81 @@ class ListNode {
 public : 
     int val; 
     ListNode* next; 
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int val) : val(val), next(nullptr) {} 
+    ListNode(int val, ListNode* node) : val(val), next(node) {}
 }; 
 
 
 class List {
 public : 
     ListNode* head; 
+    int size; 
+    // 构造函数
+    List() : head(nullptr) , size(0) {}
+    
+    // 头插
+    void insertAthead(int val) {
+        ListNode* newhead  = new ListNode(val); 
+        newhead->next = head; 
+        head = newhead; 
+        size++; 
+    }
+
+
+    // 尾插 
+    void insertAttail(int val) {
+        ListNode* newnode = new ListNode(val); 
+        if (head == nullptr) {
+            head = newnode; 
+        } else {
+            ListNode* curr = head; 
+            while (curr->next != nullptr) {
+                curr = curr->next; 
+            } 
+            curr->next = newnode; 
+        }
+        size++; 
+    } 
+
+    // 在任意位置插入节点
+    void insert(int index, int val) { 
+        if (index == 0) {
+            insertAthead(val); 
+            return; 
+        }
+        ListNode* node = new ListNode(val); 
+        
+        ListNode* curr = head; 
+        for (int i = 1; i < index; i++) {
+            curr = curr->next; 
+        } 
+        node->next = curr->next; 
+        curr->next = node; 
+        size++; 
+    } 
+
+    // 删除头节点
+    void pop_front() { 
+        if (head == nullptr) {
+            return; 
+        }
+        ListNode* temp = head; 
+        head = head->next; 
+        delete temp; 
+        size--; 
+    } 
+
+
+
+    // 删除尾节点
+    void pop_back() {
+        if (head == nullptr) {
+            return;  
+        }  
+        if (head->next == nullptr) { 
+            dele
+        }
+        ListNode* curr = 
+    }
 }; 
