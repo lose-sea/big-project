7ee548855248434a71ac9d86005b0fd0c0a44a0b
lose_sea <2031938936@qq.com>
2025-12-17 01:02:30 +0800

    Update 通讯录.cpp

--- 通讯录/通讯录.cpp
+++ 通讯录/通讯录.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 
 
-
-
-
-
-
-
+struct 
 
 
 
@@ -19,15 +14,48 @@ void showMenu() {
     cout << "***************************" << endl; 
     cout << "*****  1. 添加联系人  *****" << endl; 
     cout << "*****  2. 显示联系人  *****" << endl; 
-    cout << "*****  3. 添加联系人  *****" << endl; 
-    cout << "*****  4. 修改联系人  *****" << endl; 
+    cout << "*****  3. 删除联系人  *****" << endl; 
+    cout << "*****  4. 查找联系人  *****" << endl; 
     cout << "*****  5. 修改联系人  *****" << endl; 
-    cout << "*****  6. 修改联系人  *****" << endl; 
+    cout << "*****  6. 清空联系人  *****" << endl; 
+    cout << "*****  7. 推出通讯录  *****" << endl; 
     cout << "***************************" << endl; 
 }
 
 
-int main() {
-    showMenu(); 
+int main() { 
+
+
+
+
+
+    while(1) {
+        // 显示菜单
+        showMenu(); 
+        // 创建用户选择输入的变量
+        int select = 0; 
+        cin >> select; 
+        switch(select) {
+            case 1 :        // 1. 添加联系人
+                break; 
+            case 2 :        // 2. 显示联系人 
+                break; 
+            case 3 :        // 3. 删除联系人
+                break; 
+            case 4 :        // 4. 查找联系人 
+                break; 
+            case 5 :        // 5. 修改联系人
+                break; 
+            case 6 :        // 6. 清空联系人 
+                break; 
+            case 7 :        // 7. 推出通讯录
+                cout << "欢迎下次使用" << endl; 
+                return 0; 
+                break; 
+            default : 
+                break; 
+        }
+    }
+
     return 0; 
 }
