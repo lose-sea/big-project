2d58c5d92c7d8d8059b515e8c972850077de35f6
lose_sea <2031938936@qq.com>
2025-12-27 18:53:03 +0800

    Update 学生管理系统.cpp

--- 学生管理系统/学生管理系统.cpp
+++ 学生管理系统/学生管理系统.cpp
@@ -6,35 +6,20 @@
 //
 
 
-#include<iostream> 
-#include<string> 
-using namespace std; 
+#include<stdio.h> 
 
-// 老师类
-class Teacther { 
-public: 
-    string name;        // 姓名
-    string phone;       // 电话
-    string sex;         // 性别
-    int age;            // 年龄
-    int id;             // 工号
+struct teacter {
+    char name[20]; 
+    char sex[10]; 
+    int age; 
+    int telephone; 
 }; 
 
+struct student {
+    char name; 
+    char sex; 
 
-// 学生类
-class Student {
-public: 
-    string name;        // 姓名
-    string id;          // 学号 
-    int age;            // 年龄
-    string sex;         // 性别
-    string phone;       // 电话
-    Teacther teacher[5]; // 老师 
-    int Chinese;        // 语文成绩
-    int math;           // 数学成绩
-    int English;        // 英语成绩 
-    string major;       // 专业
-};
+}; 
 
 
 
