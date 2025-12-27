d685ef661caff4a5abf7bc5a3a2a091bcbac5c7c
lose_sea <238879830+lose-sea@users.noreply.github.com>
2025-12-27 20:48:07 +0800

    test

---
+++ 学生管理系统/学生管理系统.c
@@ -0,0 +1,75 @@
+//
+//  学生管理系统.c
+//  
+//
+//  Created by lose_sea on 2025/12/16.
+//
+
+
+#include<stdio.h> 
+#include<stdlib.h> 
+
+
+
+
+// 学生
+typedef struct student { 
+    char id[__INT_MAX__]; 
+    char name[__INT_MAX__]; 
+    char sex[20]; 
+    int age; 
+    int telephone; 
+    int score; 
+    char major[__INT_MAX__]; 
+    char garde; 
+    char ; 
+}student; 
+
+int studentSize = 0; 
+struct student stu[__INT_MAX__];
+
+void showStudent() {
+    if (studentSize == 0) {
+        printf("学生数量为0\n"); 
+        return; 
+    } 
+    for (int i = 0; i < studentSize; i++) {
+        printf("%s %s %s", stu[i].name, stu[i].id, stu[i].sex); 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void displayMenu() {
+    printf("*********************\n");
+    printf("***1. 展示学生信息 ****\n");
+    printf("***2. 添加学生信息 ****\n");
+    printf("***3. 修改学生信息 ****\n");
+    printf("***4.   删除学生  ****\n");
+    printf("*********************\n");
+}
+
+
+
+
+
+
+
+
+int  main() {
+    // 显示菜单 
+    displayMenu(); 
+    char choise; 
+    scanf("%c", &choise); 
+    return 0; 
+}
